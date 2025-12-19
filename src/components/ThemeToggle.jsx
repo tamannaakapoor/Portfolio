afcd42753e88494edd0c470538f22473d618bf9d
tamannaakapoor <153374141+tamannaakapoor@users.noreply.github.com>
2025-12-19 20:19:51 +0530

    onphonetheme

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -33,8 +33,8 @@ export const ThemeToggle = () => {
   <button
     onClick={toggleTheme}
     className={cn(
-      "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-      "focus:outline-hidden"
+      "fixed top-4 right-4 md:top-5 md:right-5 z-50 p-2 rounded-full transition-colors duration-300",
+  "focus:outline-hidden"
     )}
   >
     {isDarkMode ? (
